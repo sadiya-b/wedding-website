048a66f3670910b1c688f0b23339bcd7f7afac00
sadiya-b <bhawania.s@northeastern.edu>
2022-11-15 11:52:35 -0800

    Skeleton for the project, complete crud operations for 1 feature

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+
+/*Here, we are requiring express and cors to be used. const port process.env.port will access the port variable from the config.env we required.*/
